2e3fe02e197e4eabe234a52ad0ba68f4d605083c
salihsimseek@gmail.com <salihsimseek@gmail.com>
2023-01-17 14:52:22 +0300

    worked on Language and Continen queries

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,5 +1,13 @@
 export const Query = {
     //Country queries
     countries: ((_, __, { db }) => db.countries),
-    country: ((_, { code }, { db }) => db.countries.find(country => country.code === code))
+    country: ((_, { code }, { db }) => db.countries.find(country => country.code === code)),
+
+    //Language queries
+    languages: ((_, __, { db }) => db.languages),
+    language: ((_, { code }, { db }) => db.languages.find(language => language.code === code)),
+
+    //Continent queries
+    continents: ((_, __, { db }) => db.continents),
+    continent: ((_, { code }, { db }) => db.continents.find(continent => continent.code === code))
 }
